f82cfc809727365da79d92a4cef2b5450bc0616b
Mike Bertram <contact@mibexx.de>
2020-02-28 14:08:31 +0100

    * Time including all childs

--- src/Mibexx/TimeTracking/Business/Tracking/TrackingManager.php
+++ src/Mibexx/TimeTracking/Business/Tracking/TrackingManager.php
@@ -37,7 +37,8 @@ class TrackingManager implements TrackingManagerInterface
         $this->trackings[$ident]['trackings'][$this->trackings[$ident]['current']] = [
             'start' => microtime(true),
             'end' => 0,
-            'time' => 0
+            'time' => 0,
+            'timeincl' => 0,
         ];
     }
 
@@ -54,9 +55,15 @@ class TrackingManager implements TrackingManagerInterface
             -$this->trackings[$ident]['trackings'][$this->trackings[$ident]['current']]['start'],
             5
         );
-        $this->trackings[$ident]['current']++;
 
         array_pop($this->path);
+        $parentIdent = implode('->', $this->path);
+        if ($parentIdent) {
+            $this->trackings[$parentIdent]['trackings'][$this->trackings[$parentIdent]['current']]['timeincl'] += $this->trackings[$ident]['trackings'][$this->trackings[$ident]['current']]['time']
+                                                                                                                  + $this->trackings[$ident]['trackings'][$this->trackings[$ident]['current']]['timeincl'];
+        }
+
+        $this->trackings[$ident]['current']++;
     }
 
     /**
